cd155794ce25e54a7a04c1a2871b4ac2721b66cc
Allan Knight <allkni@amazon.com>
2018-03-29 20:34:58 -0700

    Checking in source to test build system

---
+++ src/helloworld.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cs140>
+
+#include "helloworld.h"
+
+std::string edu::sbcc::cs140::HelloWorld::greetWorld() {
+	return "Hello, World!";
+}
+
+using edu::sbcc::cs140::HelloWorld;
+
+int main(UNUSED(int argc), UNUSED(char *argv[])) {
+	std::cout << HelloWorld::greetWorld() << std::endl;
+}
