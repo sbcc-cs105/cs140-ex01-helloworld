e9f8e1bb2fd671266f5ef3ea25dd611a772da6f6
Allan Knight <allkni@amazon.com>
2018-04-09 22:04:09 -0700

    Hello

--- include/helloworld.h
+++ include/helloworld.h
@@ -3,13 +3,13 @@
 
 /**
  * CS 140 Object-Oriented Programming Using C++
- * CRN: [CHANGE THIS TO YOUR INFORMATION]
+ * CRN: [CHANGE THS TO YOUR INFORMATION]
  * Assignment: HelloWorld
  *
  * Statement of code ownership: I hereby state that I have written all of this
  * code and I have not copied this code from any other person or source.
  *
- * @author [CHANGE THIS TO YOUR INFORMATION]
+ * @author [CHANGE THIS TO YOUR IFORMATION]
  */
 
 #include <string>
