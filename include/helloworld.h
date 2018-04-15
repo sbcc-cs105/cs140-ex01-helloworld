a760fbae6ad39a092161ae8883fa7a6aab804bf0
Allan Knight <allkni@amazon.com>
2018-04-14 21:35:01 -0700

    Updated header comment

--- include/helloworld.h
+++ include/helloworld.h
@@ -3,13 +3,13 @@
 
 /**
  * CS 140 Object-Oriented Programming Using C\+\+
- * CRN: [CHANGE THIS TO YOUR INFORMATION]
+ * CRN: My CRN
  * Assignment: HelloWorld
  *
  * Statement of code ownership: I hereby state that I have written all of this
  * code and I have not copied this code from any other person or source\.
  *
- * @author [CHANGE THIS TO YOUR INFORMATION]
+ * @author allkni@amazon.com
  */
 
 #include <string>
