f6ad4d7bc5b85fa8ee0a0838b1be819cb0660c87
Allan Knight <ahknight@pipeline.sbcc.edu>
2018-04-14 21:46:24 -0700

    Updated header comment again

--- include/helloworld.h
+++ include/helloworld.h
@@ -9,7 +9,7 @@
  * Statement of code ownership: I hereby state that I have written all of this
  * code and I have not copied this code from any other person or source.
  *
- * @author allkni@amazon.com
+ * @author Allan Knight
  */
 
 #include <string>
